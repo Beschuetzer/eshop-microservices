501a164f093f15732b5214bacc328b0f3086f6f4
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-06-25 14:34:55 -0500

    chore: setup marten config options and connection string

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -4,6 +4,10 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(config =>
+{
+    config.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 var app = builder.Build();
 app.MapCarter();
 app.Run();
